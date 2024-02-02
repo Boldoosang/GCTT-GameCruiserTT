31c909baf499657325e3106afac28bc94eb57cfb
Justin Baldeosingh <jbaldeo@yahoo.com>
2024-02-02 08:04:33 -0400

    Updated backend url

--- frontend/src/util/server.js
+++ frontend/src/util/server.js
@@ -1,5 +1,5 @@
 import { SERVER_ADDRESS, SERVER_PORT } from "../config/config";
 
 export function getBackendURL() {
-  return `https://gctt.justinbaldeo.com`;
+  return `https://gctt-be.justinbaldeo.com`;
 }
